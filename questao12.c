80088a40ce8f2859b01ac3dc3dfe82efcb09e49f
Juan Oliveira <juangustavo306@gmail.com>
2021-11-11 13:17:10 -0300

    questao 12

---
+++ questao12.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int soma(int a, int b){
+    int soma = a+b;
+    return soma;
+}
+
+int subtracao(int a, int b){
+    int sub = a-b;
+    return sub;
+}
+
+int operacao(int a, int b, int (*op)(int,int)){
+    int x = op(a,b);
+    return x;
+}
+
+int main (){
+
+    int a = operacao(7, 5, soma);
+    int b = operacao(7, 5, subtracao);
+
+    printf("Soma = %d\n", a);
+    printf("Diferença = %d", b);    
+}
