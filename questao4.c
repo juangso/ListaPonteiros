23ab3a02319c1ced6ca44e686fe6c49d60dc1a37
juangso <93835771+juangso@users.noreply.github.com>
2021-11-22 21:35:00 -0300

    Delete questao4.c

--- questao4.c
+++
@@ -1,8 +0,0 @@
-/*  a. p = &i --> LEGAL
-    b. *q = &j --> LEGAL
-    c. p = &*&i --> LEGAL
-    d. i = (*&)j --> ILEGAL
-    e. i = *&j --> LEGAL
-    f. i = &&j --> ILEGAL
-    g. q = *p --> LEGAL
-    h. i = (*p) + *q --> LEGAL */
