13cbd01b1dbc8062fec5bef084a92edd05faa9d4
Juan Oliveira <juangustavo306@gmail.com>
2021-11-23 21:38:07 -0300

    q11

---
+++ questao11.c
@@ -0,0 +1,8 @@
+/*  a) aloha[2] = value --> VÁLIDO;
+    b) scanf("%f", &aloha) --> VÁLIDO;
+    c) aloha = "value" --> VÁLIDO;
+    d) printf("%f", aloha) --> INVÁLIDO;
+    e) coisas[4][4] = aloha[3] --> VÁLIDO;
+    f) coisas[5] = aloha --> INVÁLIDO;
+    g) pf = value --> INVÁLIDO;
+    h) pf = aloha --> VÁLIDO*/
